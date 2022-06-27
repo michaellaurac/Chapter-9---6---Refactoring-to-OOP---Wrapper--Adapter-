7554bde7eeddc1df713476c49c4ccf2b38d0008d
Michaël L <michaellaurac@gmail.com>
2022-06-27 09:25:28 +0200

    Initial setup, class template and filename testing

---
+++ person_test.js
@@ -0,0 +1,18 @@
+const fileName = () => {
+  const theError = new Error("here I am");
+  return /\\(\w+\.js):/.exec(theError.stack)[1];
+};
+
+/* eslint-env mocha */
+
+const wish = require("wish");
+const Person = require("./person.js");
+
+describe("tests run on the 'person.js' file:", () => {
+  // setup test
+  it("verifies the test file name", () => {
+    wish(fileName() === "person_test.js");
+  });
+  // functional tests
+  console.log(Person);
+});
